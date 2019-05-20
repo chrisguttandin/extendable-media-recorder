5545dbd79649d33e56a7fc3cfe33a6a67e13de8b
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-05-20 03:02:13 +0200

    test: disable integration tests in Chrome on Travis

--- test/integration/module.js
+++ test/integration/module.js
@@ -10,128 +10,133 @@ describe('module', () => {
         await register(port);
     });
 
-    for (const mimeType of [ 'audio/wav', 'audio/webm' ]) {
+    // @todo There is currently no way to disable the autoplay policy on BrowserStack or Sauce Labs.
+    if (!(process.env.TRAVIS && /Chrome/.test(navigator.userAgent))) { // eslint-disable-line no-undef
 
-        describe(`with the mimeType of ${ mimeType }`, () => {
+        for (const mimeType of [ 'audio/wav', 'audio/webm' ]) {
 
-            let audioContext;
-            let bufferLength;
-            let mediaRecorder;
-            let mediaStream;
+            describe(`with the mimeType of ${ mimeType }`, () => {
 
-            afterEach(() => audioContext.close());
+                let audioContext;
+                let bufferLength;
+                let mediaRecorder;
+                let mediaStream;
 
-            beforeEach(function (done) {
-                this.timeout(3000);
+                afterEach(() => audioContext.close());
 
-                audioContext = new AudioContext();
-                bufferLength = 100;
+                beforeEach(function (done) {
+                    this.timeout(3000);
 
-                mediaStream = createMediaStream(audioContext, audioContext.sampleRate / bufferLength);
-                mediaRecorder = new MediaRecorder(mediaStream, { mimeType });
+                    audioContext = new AudioContext();
+                    bufferLength = 100;
 
-                // Wait two seconds before starting the recording.
-                setTimeout(done, 2000);
-            });
+                    mediaStream = createMediaStream(audioContext, audioContext.sampleRate / bufferLength);
+                    mediaRecorder = new MediaRecorder(mediaStream, { mimeType });
 
-            it('should abort the encoding when adding a track', function (done) {
-                this.timeout(10000);
-
-                let err = null;
-
-                mediaRecorder.addEventListener('dataavailable', () => {
-                    expect(err.code).to.equal(13);
-                    expect(err.name).to.equal('InvalidModificationError');
-
-                    done();
+                    // Wait two seconds before starting the recording.
+                    setTimeout(done, 2000);
                 });
 
-                mediaRecorder.addEventListener('error', ({ error }) => {
-                    err = error;
-                });
+                it('should abort the encoding when adding a track', function (done) {
+                    this.timeout(10000);
 
-                mediaRecorder.start();
+                    let err = null;
 
-                setTimeout(() => {
-                    mediaStream.addTrack(createMediaStream(audioContext).getAudioTracks()[0]);
-                }, 1000);
-            });
-
-            it('should abort the encoding when removing a track', function (done) {
-                this.timeout(10000);
-
-                let err = null;
-
-                mediaRecorder.addEventListener('dataavailable', () => {
-                    expect(err.code).to.equal(13);
-                    expect(err.name).to.equal('InvalidModificationError');
-
-                    done();
-                });
-
-                mediaRecorder.addEventListener('error', ({ error }) => {
-                    err = error;
-                });
-
-                mediaRecorder.start();
-
-                setTimeout(() => {
-                    mediaStream.removeTrack(mediaStream.getAudioTracks()[0]);
-                }, 1000);
-            });
-
-            if (mimeType === 'audio/wav') {
-
-                it('should encode a mediaStream', function (done) {
-                    this.timeout(20000);
-
-                    mediaRecorder.addEventListener('dataavailable', async ({ data }) => {
-                        // Test if the arrayBuffer is decodable.
-                        const arrayBuffer = await new Promise((resolve) => {
-                            const fileReader = new FileReader();
-
-                            fileReader.onload = () => resolve(fileReader.result);
-                            fileReader.readAsArrayBuffer(data);
-                        });
-                        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-
-                        // Test if the audioBuffer is at least half a second long.
-                        expect(audioBuffer.duration).to.above(0.5);
-
-                        // Test if the audioBuffer contains the ouput of the oscillator.
-                        const rotatingBuffers = [ new Float32Array(bufferLength), new Float32Array(bufferLength) ];
-
-                        for (let i = 0; i < audioBuffer.numberOfChannels; i += 1) {
-                            audioBuffer.copyFromChannel(rotatingBuffers[0], i);
-
-                            for (let startInChannel = bufferLength; startInChannel < audioBuffer.length - bufferLength; startInChannel += bufferLength) {
-                                audioBuffer.copyFromChannel(rotatingBuffers[1], i, startInChannel);
-
-                                for (let j = 0; j < bufferLength; j += 1) {
-                                    try {
-                                        expect(rotatingBuffers[0][j]).to.not.equal(0);
-                                        expect(rotatingBuffers[0][j]).to.be.closeTo(rotatingBuffers[1][j], 0.0001);
-                                    } catch (err) {
-                                        done(err);
-
-                                        return;
-                                    }
-                                }
-
-                                rotatingBuffers.push(rotatingBuffers.shift());
-                            }
-                        }
+                    mediaRecorder.addEventListener('dataavailable', () => {
+                        expect(err.code).to.equal(13);
+                        expect(err.name).to.equal('InvalidModificationError');
 
                         done();
                     });
+
+                    mediaRecorder.addEventListener('error', ({ error }) => {
+                        err = error;
+                    });
+
                     mediaRecorder.start();
 
-                    setTimeout(() => mediaRecorder.stop(), 1000);
+                    setTimeout(() => {
+                        mediaStream.addTrack(createMediaStream(audioContext).getAudioTracks()[0]);
+                    }, 1000);
                 });
 
-            }
+                it('should abort the encoding when removing a track', function (done) {
+                    this.timeout(10000);
 
-        });
+                    let err = null;
+
+                    mediaRecorder.addEventListener('dataavailable', () => {
+                        expect(err.code).to.equal(13);
+                        expect(err.name).to.equal('InvalidModificationError');
+
+                        done();
+                    });
+
+                    mediaRecorder.addEventListener('error', ({ error }) => {
+                        err = error;
+                    });
+
+                    mediaRecorder.start();
+
+                    setTimeout(() => {
+                        mediaStream.removeTrack(mediaStream.getAudioTracks()[0]);
+                    }, 1000);
+                });
+
+                if (mimeType === 'audio/wav') {
+
+                    it('should encode a mediaStream', function (done) {
+                        this.timeout(20000);
+
+                        mediaRecorder.addEventListener('dataavailable', async ({ data }) => {
+                            // Test if the arrayBuffer is decodable.
+                            const arrayBuffer = await new Promise((resolve) => {
+                                const fileReader = new FileReader();
+
+                                fileReader.onload = () => resolve(fileReader.result);
+                                fileReader.readAsArrayBuffer(data);
+                            });
+                            const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+                            // Test if the audioBuffer is at least half a second long.
+                            expect(audioBuffer.duration).to.above(0.5);
+
+                            // Test if the audioBuffer contains the ouput of the oscillator.
+                            const rotatingBuffers = [ new Float32Array(bufferLength), new Float32Array(bufferLength) ];
+
+                            for (let i = 0; i < audioBuffer.numberOfChannels; i += 1) {
+                                audioBuffer.copyFromChannel(rotatingBuffers[0], i);
+
+                                for (let startInChannel = bufferLength; startInChannel < audioBuffer.length - bufferLength; startInChannel += bufferLength) {
+                                    audioBuffer.copyFromChannel(rotatingBuffers[1], i, startInChannel);
+
+                                    for (let j = 0; j < bufferLength; j += 1) {
+                                        try {
+                                            expect(rotatingBuffers[0][j]).to.not.equal(0);
+                                            expect(rotatingBuffers[0][j]).to.be.closeTo(rotatingBuffers[1][j], 0.0001);
+                                        } catch (err) {
+                                            done(err);
+
+                                            return;
+                                        }
+                                    }
+
+                                    rotatingBuffers.push(rotatingBuffers.shift());
+                                }
+                            }
+
+                            done();
+                        });
+                        mediaRecorder.start();
+
+                        setTimeout(() => mediaRecorder.stop(), 1000);
+                    });
+
+                }
+
+            });
+
+        }
 
     }
 
