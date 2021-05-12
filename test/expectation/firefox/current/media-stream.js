e451bd255e9a041a72e9fe865ebb0bc9895524f2
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-05-12 12:51:01 +0200

    test: add expectation tests for channelCount in settings

---
+++ test/expectation/firefox/current/media-stream.js
@@ -0,0 +1,20 @@
+import { AudioContext } from 'standardized-audio-context';
+import { createMediaStreamWithAudioTrack } from '../../../helpers/create-media-stream-with-audio-track';
+
+describe('MediaStream', () => {
+    let audioContext;
+    let mediaStream;
+
+    afterEach(() => audioContext.close());
+
+    beforeEach(async () => {
+        audioContext = new AudioContext();
+        mediaStream = await createMediaStreamWithAudioTrack(audioContext);
+    });
+
+    // bug #15
+
+    it('should not expose the channelCount as part of the settings', () => {
+        expect(mediaStream.getAudioTracks()[0].getSettings().channelCount).to.be.undefined;
+    });
+});
