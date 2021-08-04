66934770fc78c0906df24bb3393f6e98d67c494d
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-08-04 12:08:24 +0200

    test: add createMediaStreamAudioDestinationNode() helper

---
+++ test/helpers/create-media-stream-audio-destination-node.js
@@ -0,0 +1,23 @@
+import { MediaStreamAudioDestinationNode, OscillatorNode } from 'standardized-audio-context';
+
+export const createMediaStreamAudioDestinationNode = (audioContext, channelCount = 2, frequency = 440) => {
+    const mediaStreamAudioDestinationNode = new MediaStreamAudioDestinationNode(audioContext, { channelCount });
+    const oscillatorNode = new OscillatorNode(audioContext, { frequency });
+
+    oscillatorNode.connect(mediaStreamAudioDestinationNode);
+    oscillatorNode.start();
+
+    const stream = mediaStreamAudioDestinationNode.stream;
+
+    return new Promise((resolve) => {
+        const intervalId = setInterval(() => {
+            const channelCountOfStream = stream.getAudioTracks()[0].getSettings().channelCount;
+
+            // Bug #15: Firefox and Safari do not expose the channelCount yet.
+            if (channelCountOfStream === undefined || channelCountOfStream === channelCount) {
+                clearInterval(intervalId);
+                resolve(mediaStreamAudioDestinationNode);
+            }
+        });
+    });
+};
