55a4f9be5a58a062b4dbd3d0fd924f2a27756c16
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-04-16 21:55:51 +0200

    test: update expectation tests for Chrome Canary v114

--- test/expectation/chrome/canary/media-recorder.js
+++ test/expectation/chrome/canary/media-recorder.js
@@ -1,6 +1,5 @@
 import { AudioContext } from 'standardized-audio-context';
 import { createMediaStreamWithAudioTrack } from '../../../helpers/create-media-stream-with-audio-track';
-import { createMediaStreamWithVideoTrack } from '../../../helpers/create-media-stream-with-video-track';
 
 describe('module', () => {
     describe('with a MediaStream which contains an audio track', () => {
@@ -44,33 +43,6 @@ describe('module', () => {
                 });
 
             expect((await Promise.all(Array.from({ length: 200 }, recordAboutASecondOfAudio))).sort().pop()).to.be.above(1);
-        });
-    });
-
-    describe('with a MediaStream which contains a video track', () => {
-        let mediaStream;
-        let mediaRecorder;
-
-        afterEach(() => mediaStream.getTracks().forEach((track) => track.stop()));
-
-        beforeEach(() => {
-            mediaStream = createMediaStreamWithVideoTrack();
-            mediaRecorder = new MediaRecorder(mediaStream, { mimeType: 'audio/webm' });
-        });
-
-        // bug #6
-
-        it('should emit a blob without any data', function (done) {
-            this.timeout(10000);
-
-            mediaRecorder.addEventListener('dataavailable', ({ data }) => {
-                expect(data.size).to.equal(0);
-
-                done();
-            });
-            mediaRecorder.start();
-
-            setTimeout(() => mediaRecorder.stop(), 1000);
         });
     });
 });
