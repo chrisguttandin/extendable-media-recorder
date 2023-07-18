bb198accfc94ee4cb4b9454c757778416d5506d4
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-07-19 01:41:30 +0200

    fix: remove inferable type definition

--- src/factories/is-supported-promise.ts
+++ src/factories/is-supported-promise.ts
@@ -21,7 +21,7 @@ export const createIsSupportedPromise: TIsSupportedPromiseFactory = (window) => 
                 return resolve(true);
             }
 
-            const canvasElement: HTMLCanvasElement & { captureStream?(): MediaStream } = window.document.createElement('canvas');
+            const canvasElement = window.document.createElement('canvas');
 
             // @todo https://bugzilla.mozilla.org/show_bug.cgi?id=1388974
             canvasElement.getContext('2d');
