278751a23acc0eb54326fba54bb8330fcf593f64
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-02-07 00:56:03 +0100

    test: remove superfluous suffix

--- test/unit/factories/read-variable-size-integer.js
+++ test/unit/factories/read-variable-size-integer.js
@@ -1,6 +1,30 @@
 import { createReadVariableSizeInteger } from '../../../src/factories/read-variable-size-integer';
 import { readVariableSizeIntegerLength } from '../../../src/functions/read-variable-size-integer-length';
 
+/*
+ * THIS IS JUST HERE TO DOUBLECHECK MY IMPLEMENTATION
+ * const readVint = (dataView, start = 0) => {
+ *     const length = 8 - Math.floor(Math.log2(dataView.getUint8(start)));
+ *     if (length > 8) {
+ *         throw new Error(`Unrepresentable length: ${length}`);
+ *     }
+ *     if (start + length > dataView.byteLength) {
+ *         return null;
+ *     }
+ *     let value = dataView.getUint8(start) & ((1 << (8 - length)) - 1);
+ *     for (let i = 1; i < length; i += 1) {
+ *         if (i === 7) {
+ *         if (value >= 2 ** 8 && dataView.getUint8(start + 7) > 0) {
+ *             return { length, value: -1 };
+ *         }
+ *         }
+ *         value *= 2 ** 8;
+ *         value += dataView.getUint8(start + i);
+ *     }
+ *     return { length, value };
+ * }
+ */
+
 describe('readVariableSizeInteger()', () => {
     let dataView;
     let readVariableSizeInteger;
