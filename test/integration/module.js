1b493aaa507819ba26a26f7dad5534940698b76b
lionelv <lionel.vigier@kry.se>
2024-04-18 15:26:30 +0200

    test: test deregister()

--- test/integration/module.js
+++ test/integration/module.js
@@ -1,5 +1,5 @@
 import { AudioContext, ConstantSourceNode } from 'standardized-audio-context';
-import { MediaRecorder, isSupported, register } from '../../src/module';
+import { MediaRecorder, deregister, isSupported, register } from '../../src/module';
 import { connect } from 'extendable-media-recorder-wav-encoder';
 import { createMediaStreamAudioDestinationNode } from '../helpers/create-media-stream-audio-destination-node';
 import { createMediaStreamWithAudioTrack } from '../helpers/create-media-stream-with-audio-track';
@@ -31,8 +31,12 @@ describe('module', () => {
     // eslint-disable-next-line no-undef
     if (!process.env.TARGET || !process.env.TARGET.endsWith('-unsupported')) {
         describe('MediaRecorder', () => {
-            before(async () => {
-                const port = await connect();
+            let port;
+
+            afterEach(() => deregister(port));
+
+            beforeEach(async () => {
+                port = await connect();
 
                 await register(port);
             });
