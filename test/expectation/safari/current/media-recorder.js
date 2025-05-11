94c0b79e0f7c5fd63eac638bb947fe8697eade85
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-05-11 02:55:00 +0200

    test: track audio/mp4 support with pcm as codec

--- test/expectation/safari/current/media-recorder.js
+++ test/expectation/safari/current/media-recorder.js
@@ -5,5 +5,11 @@ describe('MediaRecorder', () => {
         it('should not support audio/mp4 with alac as codec', () => {
             expect(MediaRecorder.isTypeSupported('audio/mp4; codecs=alac')).to.be.false;
         });
+
+        // #25
+
+        it('should not support audio/mp4 with pcm as codec', () => {
+            expect(MediaRecorder.isTypeSupported('audio/mp4; codecs=pcm')).to.be.false;
+        });
     });
 });
