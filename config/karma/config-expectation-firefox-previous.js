8f80e5c6e5c4b3df5386494a724829d93b2efcef
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-08-25 21:23:31 +0200

    test: update expectation tests for Firefox v142

--- config/karma/config-expectation-firefox-previous.js
+++ config/karma/config-expectation-firefox-previous.js
@@ -23,7 +23,7 @@ module.exports = (config) => {
         customLaunchers: {
             PreviousFirefoxHeadlessWithPrefs: {
                 base: 'FirefoxHeadless',
-                command: 'firefox-v140/firefox/Firefox.app/Contents/MacOS/firefox',
+                command: 'firefox-v141/firefox/Firefox.app/Contents/MacOS/firefox',
                 prefs: {
                     'media.autoplay.default': 0
                 }
