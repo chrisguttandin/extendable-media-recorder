8bc406359dcf83c74da01cf6db22c6a810f6b7e0
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-03-17 23:36:08 +0100

    test: update expectation tests for Chrome v145

--- config/vitest/expectation-chrome-penultimate.ts
+++ config/vitest/expectation-chrome-penultimate.ts
@@ -13,7 +13,7 @@ export default defineConfig({
                         capabilities: {
                             'goog:chromeOptions': {
                                 args: ['--autoplay-policy=no-user-gesture-required', '--headless'],
-                                binary: 'chromium-v142/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+                                binary: 'chrome-v143/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing'
                             }
                         }
                     })
