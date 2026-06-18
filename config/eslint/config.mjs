2edb96fc1237373be2958a060422455923edc109
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-06-18 14:04:14 +0200

    build(config): update linting configuration for ESLint v9

---
+++ config/eslint/config.mjs
@@ -0,0 +1,35 @@
+import config from 'eslint-config-holy-grail';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig([
+    { extends: [config], languageOptions: { globals: { ...globals.node } }, rules: { 'no-sync': 'off', 'node/no-missing-require': 'off' } },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: [
+                        'config/vitest/expectation-chrome-canary.ts',
+                        'config/vitest/expectation-chrome-current.ts',
+                        'config/vitest/expectation-chrome-penultimate.ts',
+                        'config/vitest/expectation-chrome-previous.ts',
+                        'config/vitest/expectation-firefox-current.ts',
+                        'config/vitest/expectation-firefox-developer.ts',
+                        'config/vitest/expectation-firefox-penultimate.ts',
+                        'config/vitest/expectation-firefox-previous.ts',
+                        'config/vitest/expectation-safari-current.ts',
+                        'config/vitest/expectation-safari-penultimate.ts',
+                        'config/vitest/expectation-safari-previous.ts',
+                        'config/vitest/integration.ts',
+                        'config/vitest/unit.ts'
+                    ],
+                    // eslint-disable-next-line camelcase
+                    maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 13
+                }
+            }
+        },
+        rules: { '@typescript-eslint/strict-boolean-expressions': 'off' }
+    }
+]);
