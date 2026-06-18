577df98828615e30d3bdee6f317e0dbbc156e45c
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-06-18 15:57:25 +0200

    test: update expectation tests for Chrome Canary v151

--- test/expectation/chrome/canary/media-recorder.js
+++ test/expectation/chrome/canary/media-recorder.js
@@ -1,28 +1,6 @@
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import { AudioContext } from 'standardized-audio-context';
-import { recordAboutASecondOfAudio } from '../../../helpers/record-about-a-second-of-audio';
+import { describe, expect, it } from 'vitest';
 
 describe('MediaRecorder', () => {
-    describe('with a MediaStream which contains an audio track', () => {
-        let audioContext;
-
-        afterEach(() => {
-            audioContext.close();
-        });
-
-        beforeEach(() => {
-            audioContext = new AudioContext();
-        });
-
-        // bug #9
-
-        it('should fire another dataavailable event after being inactive', async () => {
-            expect(
-                (await Promise.all(Array.from({ length: 2000 }, () => recordAboutASecondOfAudio(audioContext)))).sort().pop()
-            ).to.be.above(1);
-        });
-    });
-
     describe('isTypeSupported()', () => {
         // bug #28
 
